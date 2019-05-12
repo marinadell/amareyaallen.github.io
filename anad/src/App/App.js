79cf9519c1fd222dd68a9830035b8ab44464f91e
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-05-12 15:03:43 -0500

    added react and header

---
+++ anad/src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="First">
+      
+      </div>
+      <div className="Second">
+      <img src="./images/amareya.jpg" alt="Smiley face" className="header-img"></img>
+      </div>
+      <div className="Third">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ anad/src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+      <header className="App-header">
+        <h1>AMAREYA ALLEN-DABNEY</h1>
+        <p>a.allendabney@gmail.com</p> 
+        <p>https://github.com/marinadell</p>
+        <p>(651)-338-6794</p>
+      </header>
+  );
+}
+
+export default Header;
